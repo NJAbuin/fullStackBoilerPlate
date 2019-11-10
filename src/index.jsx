88cd635badc58f7ef7d70dbcd4a885d93554cf5d
NJAbuin <nabuin@netuniversecorp.com>
2019-11-10 13:49:26 -0300

    erased duplicate htmls, now uses css not sass

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.scss";
+import "./styles.css";
 import Main from "./components/Main";
 
 ReactDOM.render(<Main />, document.getElementById("root")); //ADD NEW COMPONENTS TO MAIN
